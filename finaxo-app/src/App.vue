e7cfaf6736154c67c58f8318a2b633a1df484fb5
tigernaxo <tigernaxo.co.cc@gmail.com>
2022-02-08 21:55:01 +0800

    init

---
+++ finaxo-app/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+   <div class="text-h4">
+      Title
+      <q-badge align="top">cli v1.0.0</q-badge>
+    </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ finaxo-app/src/quasar.conf.ts
@@ -0,0 +1,3 @@
+build: {
+  rtl: true
+}
