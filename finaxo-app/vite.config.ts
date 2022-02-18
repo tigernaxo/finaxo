1b243db582331f528e4a04c2279415c012c3ef83
F734 <f734@ktgh.com.tw>
2022-02-18 13:39:08 +0800

    configure front-end proxy server

--- finaxo-app/vite.config.ts
+++ finaxo-app/vite.config.ts
@@ -4,6 +4,11 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy: {
+      '/api': ' http://localhost:5132'
+    }
+  },
   plugins: [
     vue({
       template: { transformAssetUrls }
